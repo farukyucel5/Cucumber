0f18aa91dec5bc43991ec832ce93711f8fde576c
Faruk Yucel <farukyucel5@gmail.com>
2022-12-12 22:27:47 +0300

    updating

--- src/test/java/Features/Login.feature
+++ src/test/java/Features/Login.feature
@@ -2,6 +2,9 @@ Feature:Login feature
 
   Scenario: Login with correct username and password
     Given navigating to the login page
-    And entering the username as "Faruk" and password as "12068036"
+    And entering the following details for the login
+      | username | password |
+      | Faruk    | 12068036 |
+
     And Clicking the login button
     Then you are supposed to see the user form page
--- src/test/java/Steps/MyStepdefs.java
+++ src/test/java/Steps/MyStepdefs.java
@@ -1,19 +1,19 @@
 package Steps;
 
+
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+
+import java.util.List;
+
 
 public class MyStepdefs {
     @Given("navigating to the login page")
     public void navigatingToTheLoginPage() {
     }
 
-    @And("entering the username as {string} and password as {string}")
-    public void enteringTheUsernameAsAndPasswordAs(String username, String password) {
-
-
-    }
 
     @And("Clicking the login button")
     public void clickingTheLoginButton() {
@@ -21,5 +21,17 @@ public class MyStepdefs {
 
     @Then("you are supposed to see the user form page")
     public void youAreSupposedToSeeTheUserFormPage() {
+    }
+
+    @And("entering the following details for the login")
+    public void enteringTheFollowingDetailsForTheLogin(DataTable data) {
+        System.out.println("The value is : " + data.cell(1, 0));
+        System.out.println("The value is : " + data.cell(1, 1));
+
+        List<List<String>> cells = data.cells();
+
+        System.out.println("The value is : " + cells.get(1).get(0));
+        System.out.println("The value is : " + cells.get(1).get(1));
+
     }
 }
